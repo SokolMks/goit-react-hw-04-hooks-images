8330b3685da53726b573934c578a51c8fb91aaa4
SokolMks <sokolenkomks@gmail.com>
2021-09-14 15:23:02 +0300

    Deleted old APP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import { Component } from 'react';
-import Container from './components/container/Container';
-import Searchbar from './components/searchBar/SearchBar';
-import ImageGallery from './components/imageGallery/ImageGallery';
+import React, { useEffect, useState } from "react";
+import SearchBar from './components/searchBar/SearchBar';
+import ImageGallery from "./components/imageGallery/ImageGallery";
+import ImageGalleryItem from "./components/imageGalleryItem/ImageGalleryItem";
+import Button from "./components/button/Button";
+import Modal from "./components/modal/Modal";
+import * as api from "./api/api";
 
-export default class App extends Component {
-  state = {
-    imageName: '',
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
+
+const App = () => {
+  const [pictures, setPictures] = useState([]);
+  const [request, setRequest] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [largeImageURL, setLargeImageURL] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!request) {
+      return;
+    }
+    fetchImages();
+  }, [request]);
+
+  useEffect(() => {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: "smooth",
+    });
+  }, [pictures]);
+
+  const onChangeRequest = (query) => {
+    setPictures([]);
+    setRequest(query);
+    setCurrentPage(1);
+    setError(null);
   };
 
-  handleFormSubmit = imageName => {
-    this.setState({ imageName });
+  const fetchImages = () => {
+    const option = { request, currentPage };
+    setIsLoading(true);
+    api
+      .fetchImages(option)
+      .then(
+        (picturesArr) => setPictures([...pictures, ...picturesArr]),
+        setCurrentPage(currentPage + 1)
+      )
+      .catch((error) => setError(error))
+      .finally(() => setIsLoading(false));
   };
 
-  render() {
-    return (
-      <Container>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGallery imageName={this.state.imageName} />
-      </Container>
-    );
-  }
-}
+  const onPictureClick = (url) => {
+    setLargeImageURL(url);
+    toggleModal();
+  };
+
+  const toggleModal = () => {
+    setIsModalOpen((prevState) => !prevState);
+  };
+
+  const shouldRenderLoadMoreButton = pictures.length > 0 && !isLoading;
+  return (
+    <>
+      <SearchBar onSubmit={onChangeRequest}></SearchBar>
+      {error && <p>Sorry! Somethimg went wrong. Try again, please!</p>}
+      <ImageGallery>
+        {pictures.map(({ id, webformatURL, largeImageURL }) => (
+          <ImageGalleryItem
+            key={id}
+            srcWebformat={webformatURL}
+            pictureId={id}
+            onClick={() => onPictureClick(largeImageURL)}
+          />
+        ))}
+      </ImageGallery>
+      {isModalOpen && (
+        <Modal toggleModal={toggleModal}>
+          <img src={largeImageURL} alt="" />
+        </Modal>
+      )}
+      {isLoading && (
+        <Loader
+          type="ThreeDots"
+          color="#00BFFF"
+          height={100}
+          width={100}
+          timeout={3000} //3 secs
+        />
+      )}
+      {shouldRenderLoadMoreButton && <Button  onClick={fetchImages} />}
+    </>
+  );
+};
+
+export default App;
